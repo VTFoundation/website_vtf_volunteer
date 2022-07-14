6384cc3f1e33f1200b04ca0ff1be1a6c91aa3083
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2022-07-14 21:24:26 +0530

    landing page added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,11 @@
+import { Hidden } from '@mui/material'
 import React from 'react'
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div style={{overflow: "hidden", marginBottom: 10, height: 450}}>
+        <img src='/assets/LandingPage.png' alt='Landing' height="100%" width="100%"/>
+    </div>
   )
 }
 
