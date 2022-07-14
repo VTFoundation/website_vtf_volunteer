f5aa16eed12dace0ac89f0f85cdc17cd4fa1d9a2
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2022-07-14 21:28:03 +0530

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,27 @@
+import { AppBar, Container, Box, Button, Toolbar } from '@mui/material'
 import React from 'react'
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <AppBar component="nav" sx={{ background: '#fff' }} >
+      <Container maxWidth={"lg"}>
+        <Box xs={{ flexGrow: 1 }}>
+          <Toolbar>
+            <Box sx={{ display: { flexGrow: 1 } }}>
+              <img src='/assets/logo-small.png' alt='logo' height={40} width={40} />
+            </Box>
+
+            <Box sx={{ textAlign: 'right' }}>
+              <Button>Home</Button>
+              <Button>About</Button>
+              <Button>Contact</Button>
+            </Box>
+          </Toolbar>
+        </Box>
+      </Container>
+    </AppBar>
+
   )
 }
 
